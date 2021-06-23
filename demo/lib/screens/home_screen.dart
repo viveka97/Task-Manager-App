70556bfc122c8ed54ad6ef00e382a041102011bc
Viveka Hewapathirana <57043436+viveka97@users.noreply.github.com>
2021-06-24 01:58:13 +0530

    s

--- demo/lib/screens/home_screen.dart
+++ demo/lib/screens/home_screen.dart
@@ -1,11 +1,18 @@
 import 'package:flutter/material.dart';
 
+import 'package:google_fonts/google_fonts.dart';
+
 class HomeScreen extends StatelessWidget {
   static const routeName = '/home_screen';
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Task Manager')),
+      appBar: AppBar(
+        title: Text(
+          'Task Manager',
+          style: GoogleFonts.montserrat(),
+        ),
+      ),
       body: Center(
         child: Text('No Tasks added yet!'),
       ),
@@ -18,9 +25,24 @@ class HomeScreen extends StatelessWidget {
         onPressed: () => showModalBottomSheet(
           context: context,
           builder: (BuildContext context) => Container(
-            color: Colors.blue,
+            color: Colors.blue[200],
             child: Column(
-              children: [Text('Add Task')],
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text('Add Task',
+                        style: GoogleFonts.montserrat(
+                          color: Colors.white,
+                          fontSize: 20.0,
+                        )),
+                    GestureDetector(
+                      onTap: () => Navigator.of(context).pop(),
+                      child: Icon(Icons.close),
+                    ),
+                  ],
+                )
+              ],
             ),
           ),
         ),
