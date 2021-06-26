0cf39dd73921ec745d3c1b7688c9ed6006ac0afd
Viveka Hewapathirana <57043436+viveka97@users.noreply.github.com>
2021-06-26 12:48:54 +0530

    Update home_screen.dart

--- demo/lib/screens/home_screen.dart
+++ demo/lib/screens/home_screen.dart
@@ -25,23 +25,40 @@ class HomeScreen extends StatelessWidget {
         onPressed: () => showModalBottomSheet(
           context: context,
           builder: (BuildContext context) => Container(
+            padding: const EdgeInsets.all(10.0),
             color: Colors.blue[200],
             child: Column(
               children: [
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    Text('Add Task',
-                        style: GoogleFonts.montserrat(
-                          color: Colors.white,
-                          fontSize: 20.0,
-                        )),
+                    Text(
+                      'Add Task',
+                      style: GoogleFonts.montserrat(
+                        color: Colors.white,
+                        fontSize: 20.0,
+                      ),
+                    ),
                     GestureDetector(
                       onTap: () => Navigator.of(context).pop(),
                       child: Icon(Icons.close),
                     ),
                   ],
-                )
+                ),
+                Divider(
+                  thickness: 1.2,
+                ),
+                SizedBox(
+                  height: 20.0,
+                ),
+                TextField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(5.0),
+                      borderSide: BorderSide(color: Colors.blue),
+                    ),
+                  ),
+                ),
               ],
             ),
           ),
